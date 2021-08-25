b8ddd922cab02b804d1718d9f4d029893fba0adc
Oleksandr Morozov <oleksandr.morozov@protonmail.com>
2021-08-25 17:18:38 +0300

    Updates

---
+++ precache-manifest.3db2a3b5aecbbc861482c4c539e7387d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55cc4acaee46a3a260eefdf4c0b8754a",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ff5e033a1461fd22c342",
+    "url": "./static/css/main.2277f5e7.chunk.css"
+  },
+  {
+    "revision": "a3fa78d2a47eeb043805",
+    "url": "./static/js/2.ac9e10eb.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.ac9e10eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff5e033a1461fd22c342",
+    "url": "./static/js/main.e3b8448e.chunk.js"
+  },
+  {
+    "revision": "8b3c3b96386eee1ea33a",
+    "url": "./static/js/runtime-main.b672bc02.js"
+  }
+]);
